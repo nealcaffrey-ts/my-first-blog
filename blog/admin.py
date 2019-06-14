75c5b2fd2786f475c8b79a35187e2084c3ae553e
渋谷太志 <nick@MacBook-Air.local>
2019-06-14 13:51:23 +0900

    My Django Girls app, first commit

---
+++ blog/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Post
+
+# ここでモデルを登録している
+admin.site.register(Post)
---
+++ girls/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/nick/Desktop/勉強用/Python_/django/djangogirls/girls/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
